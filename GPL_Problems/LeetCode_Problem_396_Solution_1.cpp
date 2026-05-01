44294708703e7af680e3aa11eabacd46444eb089
Omar Ayman Kishk <oaymen612@gmail.com>
2026-05-01 05:39:06 +0300

    Add LeetCode 396 solution

---
+++ GPL_Problems/LeetCode_Problem_396_Solution_1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+
+        long long sum = 0;
+        long long F = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            F += (long long)i * nums[i];
+        }
+
+        long long maxVal = F;
+
+        for (int k = 1; k < n; k++) {
+            F = F + sum - (long long)n * nums[n - k];
+            maxVal = max(maxVal, F);
+        }
+
+        return (int)maxVal;
+    }
+};
